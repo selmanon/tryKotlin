b1a03880b4d15255861e9e8d10d3443467069193
abdellah <selassi.abdellah@gmail.com>
2019-05-03 11:47:24 +0200

    Add indexOfMaximum & Sum problems solution

---
+++ src/main/java/problems/sum/Sum.kt
@@ -0,0 +1,11 @@
+package problems.sum
+
+/*
+ * Your task is to implement the problems.problems.indexofmaximum.problems.sum() function so that it computes the problems.problems.indexofmaximum.problems.sum of
+ * all elements in the given array a.
+ */
+
+fun sum(a: IntArray): Int {
+    // Write your solution here
+    return a.sum()
+}
---
+++ src/main/java/problems/sum/Tests.kt
@@ -0,0 +1,32 @@
+package problems.sum
+
+
+import org.junit.Test
+import kotlin.test.*
+import java.util.*
+
+public class Tests {
+    @Test fun testSum1() {
+        test(0)
+    }
+
+    @Test fun testSum2() {
+        test(1, 1)
+    }
+
+    @Test fun testSum3() {
+        test(-1, -1, 0)
+    }
+
+    @Test fun testSum4() {
+        test(6, 1, 2, 3)
+    }
+
+    @Test fun testSum5() {
+        test(6, 1, 1, 1, 1, 1, 1)
+    }
+}
+
+fun test(expectedSum: Int, vararg data: Int) {
+    assertEquals(expectedSum, sum(data), "\ndata = ${Arrays.toString(data)}")
+}
